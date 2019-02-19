fcd5856ec9d1da5ac30025010f101c9cf7a85dd6
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-19 13:13:39 -0500

    Update main.vb

--- main.vb
+++ main.vb
@@ -1,21 +1,24 @@
 Module Main1
 
     Sub Main()
-    
         Dim EXEArgs = My.Application.CommandLineArgs.ToString()
         Dim EXEName = "failed"
         Dim EXERen = "C:\Windows\System32\cmd.exe /k"
         EXEName = Process.GetCurrentProcess().MainModule.ToString()
         If EXEName = "failed" Then
-            MsgBox("Failed to find required DLL.", vbCritical)
+            MsgBox("Could not find the pass-through file..", vbCritical)
         Else
-            EXERen = "C:\Windows\System32\cmd.exe /k rename " + EXEName + " temp.exe"
-            Process.Start(EXERen)
-            Process.Start("C:\Windows\temp.exe " + EXEArgs(1) + EXEArgs(2) + EXEArgs(3) + EXEArgs(4) + EXEArgs(5) + EXEArgs(6))
+            Try
+                EXERen = "C:\Windows\System32\cmd.exe /k rename " + EXEName + ".exe" + EXEName + ".exe"
+                Process.Start(EXERen)
+                Process.Start("C:\Windows\temp.exe " + EXEArgs(1) + EXEArgs(2) + EXEArgs(3) + EXEArgs(4) + EXEArgs(5) + EXEArgs(6))
 
-            Process.Start("C:\Windows\System32\cmd.exe /k echo y | del C:\Windows\temp.exe")
+                Process.Start("C:\Windows\System32\cmd.exe /k echo y | del C:\Windows\temp.exe")
+            Catch
+                MsgBox("Invalid File.", vbCritical)
+            End Try
         End If
     End Sub
 
 End Module
-' not written in an IDE, might not work
+' not written in an IDE
